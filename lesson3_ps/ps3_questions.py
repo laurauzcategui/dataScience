881a04fb2c854bfe293f1d1930751f6181fcb3b0
laurauzcategui <laura.uzcateguij@gmail.com>
2015-09-08 21:54:54 +0100

    working on problem set 3

---
+++ lesson3_ps/ps3_questions.py
@@ -0,0 +1,12 @@
+The image produced by your code is shown below:
+Does the data seem normally distributed?
+- The data seems to be normally distributed
+Do you think we would be able to use Welch's t-test on this data?
+- We would be able to use Welch's t-test one tail
+
+
+3.3 Is statically different between rainy and non-rainy days?
+Yes.
+
+3.4 Results and methods
+The t statistic is greater than zero and the p value is less than the p critical value, so that it fall within the critical region
